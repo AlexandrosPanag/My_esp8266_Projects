93fdfd065edc486caa59c64199f1d9b2fc408e88
Αλέξανδρος Παναγιωτακόπουλος <69578661+AlexandrosPanag@users.noreply.github.com>
2022-05-03 12:41:24 +0300

    Add files via upload

---
+++ LCD/esp8266_LCD_Time_Counter.ino
@@ -0,0 +1,36 @@
+/*********
+ By Alexandros Panagiotakopoulos
+*********/
+
+#include <LiquidCrystal_I2C.h>
+
+
+
+// set the LCD number of columns and rows
+int time_count_second=0; //Declaring a time counter
+int lcdColumns = 16;
+int lcdRows = 2;
+
+// set LCD address, number of columns and rows
+// if you don't know your display address, run an I2C scanner sketch
+LiquidCrystal_I2C lcd(0x27, lcdColumns, lcdRows);  
+
+void setup(){
+  // initialize LCD
+  lcd.init();
+  // turn on LCD backlight                      
+  lcd.backlight();
+}
+
+void loop(){
+  //random number generator
+   time_count_second++; //adding each second on the time counter
+    
+  // set cursor to first column, first row
+  lcd.setCursor(0, 0);
+  // print message
+  lcd.print(time_count_second);
+  // clears the display to print new message
+  delay(1000);
+  lcd.clear(); 
+}
