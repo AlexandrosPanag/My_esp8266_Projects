eabd51499972d2646fc2bc322d21cc8af0a89d0c
Αλέξανδρος Παναγιωτακόπουλος <69578661+AlexandrosPanag@users.noreply.github.com>
2022-05-25 15:40:34 +0300

    Update Thinger_Live.ino

--- Thinger.io/Thinger_Live.ino
+++ Thinger.io/Thinger_Live.ino
@@ -1,23 +1,23 @@
 #include <ESP8266WiFi.h>
-#include <ThingerESP8266.h>
+#include <ThingerESP8266.h> //include the neccessary libraries
 
-#define USERNAME "embeddedtvm"
-#define DEVICE_ID "IPCS"
-#define DEVICE_CREDENTIAL "NODE"
+#define USERNAME "myusername" //define your username's password from thinger.io
+#define DEVICE_ID "mydeviceid" //define your device's id from thinger.io
+#define DEVICE_CREDENTIAL "myrandomlygeneratedcredentials" //define your randomly generated credentials from thinger.io
 
-#define SSID "Redmi 7"
-#define SSID_PASSWORD "mypassword"
+#define SSID "myrouterid" //define your router's id
+#define SSID_PASSWORD "myrouterpassword" //define your router's password
 
 
-ThingerESP8266 thing(USERNAME, DEVICE_ID, DEVICE_CREDENTIAL);
+ThingerESP8266 thing(USERNAME, DEVICE_ID, DEVICE_CREDENTIAL); //generate a connection
 
 void setup() {
-  pinMode(D0, OUTPUT);
-  Serial.begin(9600);
-  thing.add_wifi(SSID, SSID_PASSWORD);
-  thing["D0"] << digitalPin(D0);
+  pinMode(0, OUTPUT); //set D0 as an output
+  Serial.begin(9600); //begin the baud rate to 9600 bauds
+  thing.add_wifi(SSID, SSID_PASSWORD); //add the wi-fi password
+  thing["0"] << digitalPin(0); //create a data output from D0
 }
 
 void loop() {
-  thing.handle();
+  thing.handle(); //generate the connection
 }
